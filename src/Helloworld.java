1a543d4931db79ea995f55e97851c00fe812ca9f
Sangram Shinde <sangram.shinde01@ad.infosys.com>
2021-04-14 05:24:30 +0530

    Initial commit

---
+++ src/Helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld {
+
+    public static void main(String args[])
+    {
+        System.out.println("welcome to the java");
+        int a=3;
+        int b=4;
+        int c=a+b;
+        System.out.println("sum ="+ c);
+    }
+}
